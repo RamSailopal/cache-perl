32d47ac4f2b912885d273c91b91589c3ea4682e2
RamSailopal <thelinuxguys@yahoo.com>
2021-04-26 09:28:55 +0100

    First commit

---
+++ files/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [[ "$1" == "" ]]
+then
+	echo "Pass either 32 or 64 as the first variable based on the chip architecture"
+        exit
+fi
+if [[ "$1" == "64" ]]
+then
+	echo "At present, the install doesn't run on 64 bit systems"
+	exit
+else
+	pat="/usr/lib"
+fi
+while read vers
+do
+                loc=$(find "$vers/site_perl" -maxdepth 2 -mindepth 2 -type d)
+                if test -e "$loc/Intersys"
+                then
+			echo "Perl libraries already exist"
+                else
+			echo "Installing Perl libraries"
+                        mkdir -p "$loc/Intersys"
+                        cp -R /usr/local/cache-perl/Intersys/* "$loc/Intersys"
+                fi
+done <<< "$(find "$pat" -maxdepth 1 -mindepth 1 -type d -name "perl*")"
---
+++ templates/testauto.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl test.pl'
+
+#########################
+
+# change 'tests => 1' to 'tests => last_test_to_print';
+
+use Test;
+BEGIN { plan tests => 3 };
+use Intersys::PERLBIND;
+ok(1); # If we made it this far, we're ok.
+
+#########################
+
+# Insert your test code below, the Test module is use()ed here so read
+# its man page ( perldoc Test ) for help writing this test script.
+$|=1; # set autoflush so output is easier to read
+$port="{{ prt }}";
+$host="{{ host }}";
+$port = 1972 if $port eq "";
+eval {
+    $conn = Intersys::PERLBIND::Connection->new("[$host][$port]:Samples","_SYSTEM","SYS",0);
+    $database = Intersys::PERLBIND::Database->new($conn);
+    $object =  $database->openid("Sample.Person", "1", -1, -1);
+    $ans = $object->run_obj_method("Addition", 17, 20);
+    ok($ans,37);
+};
+if ($@) {
+    print "test failed\n";
+    print "error message is $@\n";
+    ok(0);
+} else {
+    ok(1);
+}
